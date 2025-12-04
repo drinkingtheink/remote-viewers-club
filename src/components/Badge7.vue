8c1d79b8b1685945bdc17a3da2e64b35bd5d539f
drinkingtheink <jason.m.harrison@gmail.com>
2025-12-03 23:19:33 -0600

    oww badge wip

--- src/components/Badge7.vue
+++ src/components/Badge7.vue
@@ -1,7 +1,7 @@
 <template>
     <div 
         class="seraphim-wrapper"
-        :class="{ 'animate': doAnimate }"    
+        :class="{ 'animate2': doAnimate }"    
     >
         <svg id="seraphim" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 877.84 967.37">
             <g id="RIGHT-WING">
@@ -206,13 +206,26 @@ export default {
   height: var(--circle-size);
   border-radius: 50%;
   background-color: #065656;
-  background-image: url("data:image/svg+xml,%3Csvg width='84' height='84' viewBox='0 0 84 84' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23008b8b' fill-opacity='0.58'%3E%3Cpath d='M84 23c-4.417 0-8-3.584-8-7.998V8h-7.002C64.58 8 61 4.42 61 0H23c0 4.417-3.584 8-7.998 8H8v7.002C8 19.42 4.42 23 0 23v38c4.417 0 8 3.584 8 7.998V76h7.002C19.42 76 23 79.58 23 84h38c0-4.417 3.584-8 7.998-8H76v-7.002C76 64.58 79.58 61 84 61V23zM59.05 83H43V66.95c5.054-.5 9-4.764 9-9.948V52h5.002c5.18 0 9.446-3.947 9.95-9H83v16.05c-5.054.5-9 4.764-9 9.948V74h-5.002c-5.18 0-9.446 3.947-9.95 9zm-34.1 0H41V66.95c-5.053-.502-9-4.768-9-9.948V52h-5.002c-5.184 0-9.447-3.946-9.95-9H1v16.05c5.053.502 9 4.768 9 9.948V74h5.002c5.184 0 9.447 3.946 9.95 9zm0-82H41v16.05c-5.054.5-9 4.764-9 9.948V32h-5.002c-5.18 0-9.446 3.947-9.95 9H1V24.95c5.054-.5 9-4.764 9-9.948V10h5.002c5.18 0 9.446-3.947 9.95-9zm34.1 0H43v16.05c5.053.502 9 4.768 9 9.948V32h5.002c5.184 0 9.447 3.946 9.95 9H83V24.95c-5.053-.502-9-4.768-9-9.948V10h-5.002c-5.184 0-9.447-3.946-9.95-9zM50 50v7.002C50 61.42 46.42 65 42 65c-4.417 0-8-3.584-8-7.998V50h-7.002C22.58 50 19 46.42 19 42c0-4.417 3.584-8 7.998-8H34v-7.002C34 22.58 37.58 19 42 19c4.417 0 8 3.584 8 7.998V34h7.002C61.42 34 65 37.58 65 42c0 4.417-3.584 8-7.998 8H50z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-  z-index: -1; /* Sits behind content */
+  background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%23008b8b' fill-opacity='0.58' fill-rule='evenodd'/%3E%3C/svg%3E");  z-index: -1; /* Sits behind content */
   border: 20px solid gold;
 }
 
 .animate svg {
     animation: float 3s infinite alternate;
+}
+
+.seraphim-wrapper:hover #CLOSED-EYE {
+    visibility: hidden;
+}
+
+.seraphim-wrapper:hover #EYE-WHITES-3,
+.seraphim-wrapper:hover #EYE-SHINE-3 {
+    fill: white;
+    opacity: 1;
+}
+
+.seraphim-wrapper:hover #OPEN-EYE {
+    opacity: 1;
 }
 
 @keyframes blink {
@@ -229,7 +242,7 @@ export default {
 
 /* Main Eyes - First to blink */
 .animate #OPEN-EYE {
-  animation: blink 8s infinite;
+  animation: blink 4s infinite;
   animation-delay: 0s;
 }
 
@@ -247,26 +260,26 @@ export default {
 
 .animate #CLOSED-EYE {
   opacity: 0;
-  animation: blink-inverse 8s infinite;
+  animation: blink-inverse 4s infinite;
   animation-delay: 0s;
   fill: black;
 }
 
 /* Right Wing Eyes - Second to blink */
 .animate #RW-OPEN-EYE {
-  animation: blink 8s infinite;
+  animation: blink 4s infinite;
   animation-delay: 0.3s;
 }
 
 .animate #RW-CLOSED-EYE {
   opacity: 0;
-  animation: blink-inverse 8s infinite;
+  animation: blink-inverse 4s infinite;
   animation-delay: 0.3s;
 }
 
 /* Top Wing Eyes - Third to blink */
 .animate #TW-OPEN-EYE {
-  animation: blink 8s infinite;
+  animation: blink 4s infinite;
   animation-delay: 0.6s;
 }
 
@@ -278,7 +291,7 @@ export default {
 
 /* Left Wing Eyes - Fourth to blink */
 .animate #LW-OPEN-EYE {
-  animation: blink 8s infinite;
+  animation: blink 4s infinite;
   animation-delay: 0.9s;
 }
 
@@ -290,7 +303,7 @@ export default {
 
 /* Bottom Wing Eyes - Fifth to blink */
 .animate #BW-OPEN-EYE {
-  animation: blink 8s infinite;
+  animation: blink 4s infinite;
   animation-delay: 1.2s;
 }
 
@@ -339,6 +352,10 @@ export default {
 }
 
 .animate #main-iris-3 {
+    fill: #00FFFF;
+}
+
+.seraphim-wrapper:hover #main-iris-3 {
     fill: #00FFFF;
 }
 
